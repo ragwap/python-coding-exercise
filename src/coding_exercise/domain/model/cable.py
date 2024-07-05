c7b0b9de2d6f8779265bda53bc2df40d8d7bed12
SVO <svo@qual.is>
2024-07-06 09:43:23 +1000

    Cable are equal if they have the same length

--- src/coding_exercise/domain/model/cable.py
+++ src/coding_exercise/domain/model/cable.py
@@ -11,3 +11,9 @@ class Cable:
 
     def __init__(self, length):
         self.__set_length(length)
+
+    def __eq__(self, other):
+        if isinstance(other, Cable):
+            return self.length == other.length
+
+        return False
--- tests/coding_exercise/domain/model/test_cable.py
+++ tests/coding_exercise/domain/model/test_cable.py
@@ -25,3 +25,13 @@ def test_should_raise_error_if_negative_provided_for_length():
 
 def test_should_raise_error_if_greater_than_maxsize_provided_for_length():
     assert_that(Cable).raises(ValueError).when_called_with(sys.maxsize + 1)
+
+
+def test_should_equal():
+    given_length = 101
+
+    assert_that(Cable(given_length)).is_equal_to(Cable(given_length))
+
+
+def test_should_not_equal():
+    assert_that(Cable(101)).is_not_equal_to(Cable(1010))
