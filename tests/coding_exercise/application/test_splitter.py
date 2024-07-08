cb6d89bf437e393eafdcaae7683569159920d85d
SVO <svo@qual.is>
2024-07-08 10:40:47 +1000

    Simplification of empty test of Splitter

--- tests/coding_exercise/application/test_splitter.py
+++ tests/coding_exercise/application/test_splitter.py
@@ -1,25 +1,8 @@
-from inspect import getmembers, isfunction, signature
-
 from assertpy import assert_that
 
 from coding_exercise.application.splitter import Splitter
-
-CABLE_CLASS = "coding_exercise.domain.model.cable.Cable"
-SPLIT_SIGNATURE = f"(self, cable: {CABLE_CLASS}, times: int) -> list[{CABLE_CLASS}]"
+from coding_exercise.domain.model.cable import Cable
 
 
-def test_should_have_split_method():
-    def has_expected_split_method_signature(object):
-        return str(signature(object)) == SPLIT_SIGNATURE
-
-    def is_named_split(object):
-        return object.__name__ == "split"
-
-    def split_method(object):
-        return (
-            isfunction(object)
-            and is_named_split(object)
-            and has_expected_split_method_signature(object)
-        )
-
-    assert_that(getmembers(Splitter, predicate=split_method)).is_not_empty()
+def test_should_not_return_none_when_splitting_cable():
+    assert_that(Splitter().split(Cable(10, "coconuts"), 1)).is_not_none()
