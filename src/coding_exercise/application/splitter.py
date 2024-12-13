01654f0dacb9e520a160c13045de252f3c0b7eff
ragwap <raviandgang@gmail.com>
2024-12-13 23:15:34 +1100

    Code refactor 2 - splitter

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -15,13 +15,13 @@ class Splitter:
         if not isinstance(cable, Cable) or not isinstance(times, int):
             raise ValueError("Invalid input types")
         
-        # Checks if times and cable lenghts are within the specified ranges
+        # Checks if times and cable lengths are within the specified ranges
         if times < MIN_TIMES or times > MAX_TIMES:
             raise ValueError("Number of splits should be in range 1 to 64 inclusive")
         if cable.length < MIN_LEN or cable.length > MAX_LEN:
             raise ValueError("Cable length should be in range 2 to 1024 inclusive")
         
-        # Checks if a cable can be split into equal/valid lenghts with the given times
+        # Checks if a cable can be split into equal/valid lengths with the given times
         if cable.length // (times + 1) < 1:
             raise ValueError("Cannot split cable into valid lengths")
 
@@ -48,15 +48,10 @@ class Splitter:
 
             counter += 1
 
-        # If no remainder array: split_length can be taken into account
-        if not split_arr:
-            gcd = split_length
-            res_range = total_splits
-        else:
-            gcd = math.gcd(*split_arr) # Else: the greatest common divisor should be the length of each cable
-            res_range = cable.length // gcd # Calculating the number of cables
+        gcd = math.gcd(*split_arr) # The greatest common divisor should be the length of each cable
+        res_range = cable.length // gcd # Calculating the number of cables
 
-        # Find the number of digits in the maximum value to fill zeros
+        # Find the number of digits in the maximum value to determine zeros to fill
         padding = len(str(res_range))
 
         result = [Cable(gcd, f"{cable.name}-{i:0{padding}d}") for i in range(res_range)]
